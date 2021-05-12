1cb902c8ca55e64e145e2fa62f2ced719198aeb6
angelxtry <angelxtry@gmail.com>
2021-05-12 23:00:25 +0900

    add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [TasksModule],
+  providers: [AppService],
+  controllers: [AppController],
 })
 export class AppModule {}
