98a3598540e01d0e11c4ce20bac8e8cdb58eea7e
sean <sean@gomicorp.com>
2021-06-01 18:41:26 +0900

    welcome message 변경-1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   checkHealth() {
-    return 'Hello Auto Deploy!!!!!!!!!!!!!';
+    return 'Hello Test';
   }
 }
